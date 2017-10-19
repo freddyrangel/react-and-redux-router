2b81e3c287c7019fd12107be7422236f443d3047
Freddy Rangel <frederick.rangel@gmail.com>
2017-10-18 23:01:05 -0700

    Counter without behavior

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <h1 className='app'>Cat-Tastrophe</h1>
+    const url = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+    return <div className='app'>
+      <h1>Cat-Tastrophe</h1>
+      <Counter />
+    </div>
   }
+}
+
+function Counter() {
+  return <div className='counter'>
+    <div className='controls'>
+      <button>More</button>
+      <button>Less</button>
+    </div>
+    <img src={ url } width='200' alt='Cat'/>
+  </div>
 }
